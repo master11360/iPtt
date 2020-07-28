5e234500bcf5a2b0230b762f5212842fcfc8cdd2
ming_chien <master113600@gmail.com>
2020-07-29 01:10:59 +0800

    Initial project

---
+++ iPtt/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  iPtt
+//
+//  Created by Ming on 2020/7/29.
+//  Copyright © 2020 Ming. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
