98e596d3d396e3f59ad4ece6d446a24c6380822c
ming_chien <master113600@gmail.com>
2020-08-01 00:00:57 +0800

    Add basic account, password textfield to try login function.

--- iPtt/ContentView.swift
+++ iPtt/ContentView.swift
@@ -9,9 +9,32 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var account = ""
+    @State private var password = ""
+    
     var body: some View {
-        Text("Hello, World!")
-            .frame(maxWidth: .infinity, maxHeight: .infinity)
+        VStack {
+            Text("Hello, World!")
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+            HStack {
+                TextField("Account", text: $account)
+                Button(action: onLoginClicked) {
+                    Text("Login")
+                }.hidden()
+            }
+            HStack {
+                SecureField("Password", text: $password)
+                Button(action: onLoginClicked) {
+                    Text("Login")
+                }
+            }
+        }.padding()
+    }
+    
+    func onLoginClicked() {
+        if !account.isEmpty && !password.isEmpty {
+//            pttMgr.login(account, password)
+        }
     }
 }
 
